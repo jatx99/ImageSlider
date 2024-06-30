aa94ed0930794ec31a0d57fd94ff2c4f50345e2b
Josh Aksamit <joshua.aksamit@gmail.com>
2024-06-30 16:01:58 -0700

    setup functions in ts file,

---
+++ index.ts
@@ -0,0 +1,25 @@
+const slides = document.querySelectorAll(".slides img");
+let slideIndex: number = 0;
+let intervalId: any = null;
+
+initializeSlider();
+
+function initializeSlider(): void{
+
+    slides[slideIndex].classList.add("displaySlide");
+
+}
+
+function showSlide(index: number): void{
+
+}
+
+function prevSlide(): void{
+
+}
+
+function nextSlide(): void{
+
+}
+
+
--- out/index.js
+++ out/index.js
@@ -1,1 +1,14 @@
+const slides = document.querySelectorAll(".slides img");
+let slideIndex = 0;
+let intervalId = null;
+initializeSlider();
+function initializeSlider() {
+    slides[slideIndex].classList.add("displaySlide");
+}
+function showSlide(index) {
+}
+function prevSlide() {
+}
+function nextSlide() {
+}
 //# sourceMappingURL=index.js.map
